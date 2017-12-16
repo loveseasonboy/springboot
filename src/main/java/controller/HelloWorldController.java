ae4e83b02b5b437bf9c8a7cee55ee95015a016c1
hehd <731882714@qq.com>
2017-12-16 18:03:07 +0800

    删除不必要包

--- src/main/java/controller/HelloWorldController.java
+++ src/main/java/controller/HelloWorldController.java
@@ -1,6 +1,5 @@
 package controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
